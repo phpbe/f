b410423e13c4ef5eeb7e18b4ceb9d059949bb82a
刘一二 <8981024@qq.com>
2021-04-02 11:01:16 +0800

    Response  templateFactory

--- src/Response/Driver.php
+++ src/Response/Driver.php
@@ -2,6 +2,9 @@
 
 namespace Be\F\Response;
 
+use Be\F\Request\RequestFactory;
+use Be\F\Template\TemplateFactory;
+
 
 /**
  * Class Driver
@@ -128,6 +131,191 @@ class Driver
     public function cookie(string $key, string $value = '', int $expire = 0, string $path = '/', string $domain = '', bool $secure = false, bool $httpOnly = false)
     {
         $this->response->cookie($key, $value, $expire, $path, $domain, $secure, $httpOnly);
+    }
+
+
+    /**
+     * 成功
+     *
+     * @param string $message 消息
+     * @param string $redirectUrl 跳转网址
+     * @param int $redirectTimeout 跳转超时时长
+     */
+    public function success(string $message, string $redirectUrl = null, int $redirectTimeout = 3)
+    {
+        $this->set('success', true);
+        $this->set('message', $message);
+
+        if ($redirectUrl !== null) {
+            $this->set('redirectUrl', $redirectUrl);
+            if ($redirectTimeout > 0) $this->set('redirectTimeout', $redirectTimeout);
+        }
+
+        $request = RequestFactory::getInstance();
+        if ($request->isAjax()) {
+            $this->json();
+        } else {
+            $this->display('App.System.System.success');
+        }
+    }
+
+    /**
+     * 失败
+     *
+     * @param string $message 消息
+     * @param string $redirectUrl 跳转网址
+     * @param int $redirectTimeout 跳转超时时长
+     */
+    public function error(string $message, string $redirectUrl = null, int $redirectTimeout = 3)
+    {
+        $this->set('success', false);
+        $this->set('message', $message);
+
+        if ($redirectUrl !== null) {
+            $this->set('redirectUrl', $redirectUrl);
+            if ($redirectTimeout > 0) $this->set('redirectTimeout', $redirectTimeout);
+        }
+
+        $request = RequestFactory::getInstance();
+        if ($request->isAjax()) {
+            $this->json();
+        } else {
+            $this->display('App.System.System.error');
+        }
+    }
+
+    /**
+     * 系统异常
+     *
+     * @param \Throwable $e 错误码
+     */
+    public function exception(\Throwable $e)
+    {
+        $request = RequestFactory::getInstance();
+        if ($request->isAjax()) {
+            $this->set('success', false);
+            $this->set('message', $e->getMessage());
+            $this->set('trace', $e->getTrace());
+            $this->set('code', $e->getCode());
+            $this->json();
+        } else {
+            $this->set('e', $e);
+            $this->display('App.System.System.exception');
+        }
+    }
+
+    /**
+     * 记录历史节点
+     *
+     * @param string $historyKey 历史节点键名
+     */
+    public function createHistory(string $historyKey = null)
+    {
+        $request = RequestFactory::getInstance();
+        if ($historyKey === null) {
+            $historyKey = $request->getAppName() . '.' . $request->getControllerName();
+        }
+
+        $this->response->cookie('_history_url_'.$historyKey, $request->server('REQUEST_URI'));
+        $this->response->cookie('_history_post_'.$historyKey, serialize($request->post()));
+    }
+
+    /**
+     * 成功
+     *
+     * @param string $message 消息
+     * @param string $historyKey 历史节点键名
+     * @param int $redirectTimeout 跳转超时时长
+     */
+    public function successAndBack(string $message, string $historyKey = null, int $redirectTimeout = 3)
+    {
+        $request = RequestFactory::getInstance();
+        if ($historyKey === null) {
+            $historyKey = $request->getAppName() . '.' . $request->getControllerName();
+        }
+
+        $this->set('success', true);
+        $this->set('message', $message);
+        $this->set('historyKey', $historyKey);
+
+        $historyUrl = $request->cookie('_history_url_'.$historyKey);
+        if (!$historyUrl) $historyUrl = $request->server('HTTP_REFERER');
+        if (!$historyUrl) $historyUrl = './';
+
+        $historyPost = $request->cookie('_history_post_'.$historyKey);
+        if ($historyPost) $historyPost = unserialize($historyPost);
+
+        $this->set('historyUrl', $historyUrl);
+        $this->set('historyPost', $historyPost);
+        $this->set('redirectTimeout', $redirectTimeout);
+        $this->display('App.System.System.successAndBack');
+    }
+
+    /**
+     * 失败
+     *
+     * @param string $message 消息
+     * @param string $historyKey 历史节点键名
+     * @param int $redirectTimeout 跳转超时时长
+     */
+    public function errorAndBack(string $message, string $historyKey = null, int $redirectTimeout = 3)
+    {
+        $request = RequestFactory::getInstance();
+        if ($historyKey === null) {
+            $historyKey = $request->getAppName() . '.' . $request->getControllerName();
+        }
+
+        $this->set('success', false);
+        $this->set('message', $message);
+        $this->set('historyKey', $historyKey);
+
+        $historyUrl = $request->cookie('_history_url_'.$historyKey);
+        if (!$historyUrl) $historyUrl = $request->server('HTTP_REFERER');
+        if (!$historyUrl) $historyUrl = './';
+
+        $historyPost = $request->cookie('_history_post_'.$historyKey);
+        if ($historyPost) $historyPost = unserialize($historyPost);
+
+        $this->set('historyUrl', $historyUrl);
+        $this->set('historyPost', $historyPost);
+        $this->set('redirectTimeout', $redirectTimeout);
+        $this->display('App.System.System.errorAndBack');
+    }
+
+    /**
+     * 显示模板
+     *
+     * @param string $template 模板名
+     * @param string $theme 主题名
+     */
+    public function display(string $template = null, string $theme = null)
+    {
+        if ($template === null) {
+            $template = 'App.' . RequestFactory::getInstance()->getRoute();
+        }
+
+        $this->response->end($this->fetch($template, $theme));
+    }
+
+    /**
+     * 获取模板内容
+     *
+     * @param string $template 模板名
+     * @param string $theme 主题名
+     * @return  string
+     */
+    public function fetch(string $template, string $theme = null)
+    {
+        ob_start();
+        ob_clean();
+        $templateInstance = TemplateFactory::getInstance($template, $theme);
+        foreach ($this->data as $key => $val) {
+            $templateInstance->$key = $val;
+        }
+        $templateInstance->display();
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
     }
 
 }
---
+++ src/Template/TemplateFactory.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Be\F\Template;
+
+use Be\F\Config\ConfigFactory;
+use Be\F\Property\PropertyFactory;
+use Be\F\Runtime\RuntimeFactory;
+
+/**
+ * Template 工厂
+ */
+abstract class TemplateFactory
+{
+
+    private static $cache = [];
+
+    /**
+     * 获取指定的一个模板（单例）
+     *
+     * @param string $template 模板名
+     * @param string $theme 主题名
+     * @return Driver
+     */
+    public static function getInstance($template, $theme = null)
+    {
+        $cid = \Swoole\Coroutine::getuid();
+        $parts = explode('.', $template);
+        $type = array_shift($parts);
+        $name = array_shift($parts);
+
+        $configSystem = ConfigFactory::getInstance('System.System');
+
+        if ($theme === null) {
+            $property = PropertyFactory::getInstance($type . '.' . $name);
+            if (isset($property->theme)) {
+                $theme = $property->theme;
+            } else {
+                $theme = $configSystem->theme;
+            }
+        }
+
+        if (isset(self::$cache[$cid][$theme][$template])) return self::$cache[$cid][$theme][$template];
+
+        $runtime = RuntimeFactory::getInstance();
+        $frameworkName = $runtime->getFrameworkName();
+        $path = $runtime->getCachePath() . '/Template/' . $theme . '/' . $type . '/' . $name . '/' . implode('/', $parts) . '.php';
+        if (!file_exists($path)) {
+            TemplateHelper::update($template, $theme);
+        }
+
+        $class = 'Be\\' . $frameworkName . '\\Cache\\Template\\' . $theme . '\\' . $type . '\\' . $name . '\\' . implode('\\', $parts);
+        self::$cache[$cid][$theme][$template] = new $class();
+        return self::$cache[$cid][$theme][$template];
+    }
+
+    /**
+     * 回收资源
+     */
+    public static function release()
+    {
+        $cid = \Swoole\Coroutine::getuid();
+        unset(self::$cache[$cid]);
+    }
+
+}
